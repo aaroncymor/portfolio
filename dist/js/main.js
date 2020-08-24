fed15c1a74ba467f9ce93b1446bf07c68ddafe3a
Aaron Cymor Nemeno <anemeno@risingtide.ph>
2020-08-24 21:52:28 +0800

    Added highlight and scale of skill eery 5 seconds

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,51 +5,77 @@ AOS.init({
 
 // setTimeout(()=>{document.body.style.overflow = "auto";}, 5000);
 
-highlightSkills('.skills__html5', '.html-tech');
-highlightSkills('.skills__css', '.css-tech');
-highlightSkills('.skills__js', '.js-tech');
-highlightSkills('.skills__python', '.python-tech');
-highlightSkills('.skills__php', '.php-tech');
-highlightSkills('.skills__sql', '.sql-tech');
-highlightSkills('.skills__devops', '.devops-tech');
-highlightSkills('.skills__os', '.os-tech');
-highlightSkills('.skills__mobile', '.mobile-tech');
+var skillIndex = 0;
+skillList = [
+    '.skills__html5',
+    '.skills__css',
+    '.skills__js',
+    '.skills__python',
+    '.skills__php',
+    '.skills__sql',
+    '.skills__devops',
+    '.skills__os',
+    '.skills__mobile'
+];
+techList = [
+    '.html-tech',
+    '.css-tech',
+    '.js-tech',
+    '.python-tech',
+    '.php-tech',
+    '.sql-tech',
+    '.devops-tech',
+    '.os-tech',
+    '.mobile-tech'
+];
 
+// every x seconds, add highlight to target div, and
+// add fade/hide to target lists
+setInterval(()=>{
+    if (skillIndex < skillList.length) {
 
-function highlightSkills(targetCard, techList){
-    const card = document.querySelector(targetCard);
-    const technologies = document.querySelectorAll(`li${techList}`);
-    const notTechnologies = document.querySelectorAll(`li:not(${techList})`);
+        // index to remove added highlight and hide classes
+        // from div.skills__<name> and li.<name>-tech
+        // if current skillIndex -1 is less than 0, we assign
+        // last element index, else it will be current skill index - 1
+        var removeIndex = skillIndex - 1 < 0 
+            ? skillList.length - 1
+            : skillIndex - 1;
+        
+        document.querySelector(skillList[removeIndex])
+            .classList.remove("enlarge");
 
-    card.addEventListener('mouseover', ()=>{
-        technologies.forEach((li, index)=>{
+        // remove highlight and hide classes with removeIndex
+        document.querySelectorAll(`li${techList[removeIndex]}`)
+        .forEach((li, item)=>{
+            li.classList.remove("highlight");
+        });
+
+        document.querySelectorAll(`.skills__tags li:not(${techList[removeIndex]})`)
+        .forEach((li, item)=>{
+            li.classList.remove("inactive");
+        });
+
+        document.querySelector(skillList[skillIndex])
+            .classList.add("enlarge");
+        
+        // highlight list of target tech and fade others
+        document.querySelectorAll(`li${techList[skillIndex]}`)
+        .forEach((li, item)=>{
             li.classList.add("highlight");
         });
 
-        notTechnologies.forEach((li, index)=>{
-            li.classList.add("hide");
+        document.querySelectorAll(`.skills__tags li:not(${techList[skillIndex]})`)
+        .forEach((li, item)=>{
+            li.classList.add("inactive");
         });
-    });    
 
-    card.addEventListener('mouseout', ()=>{
-        technologies.forEach((li, index)=>{
-            li.classList.remove("highlight");
-        });
-        notTechnologies.forEach((li, index)=>{
-            li.classList.remove("hide");
-        });
-    });
-
-    // card.addEventListener('click', ()=>{
-    //     technologies.forEach((li, index)=>{
-    //         li.classList.toggle("highlight");
-    //     });
-
-    //     notTechnologies.forEach((li, index)=>{
-    //         li.classList.toggle("hide");
-    //     });
-    // });  
-}
+        skillIndex++;
+    } else {
+        // reset
+        skillIndex = 0;
+    }
+}, 5000);
 
 const projectItems = document.querySelectorAll('.projects__item');
 projectItems.forEach((projectItem, index) => {
